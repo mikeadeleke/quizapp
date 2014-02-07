7e9468ec39d5ad92e6ab51a7c80e5312af21092d
Mike Adeleke <madeleke13@gmail.com>
2014-02-06 18:36:50 -0600

    updates js

--- application.js
+++ application.js
@@ -1,74 +1,73 @@
 function MikeAdeleke() {
-  var quizName = 'Mike Adeleke';
 
   var q1 = {
     question: "Where is Mike Adeleke from?",
     answers: ["The Midwest", "The Northeast", "The Southeast", "The 'Best' Coast"],
-    correctAnswer: 0;
-    userAnswer: 0;
+    correctAnswer: 0, 
+    userAnswer: null
   };
 
   var q2 = {
     question: "What is Mike's favorite programming language?",
     answers: ["Javascript", "Python", "Ruby", "PHP"],
-    correctAnswer: 2;
-    userAnswer: 0;
+    correctAnswer: 2,
+    userAnswer: null
   };
 
   var q3 = {
     question: "Who is his favorite team?",
     answers: ["Chelsea FC", "Arsenal", "Manchster United", "Barcelona"],
-    correctAnswer: 0;
-    userAnswer: 0;
+    correctAnswer: 0,
+    userAnswer: null
   };
 
   var q4 = {
     question: "What does his startup do?",
     answers: ["Easy finding of college textbooks", "Helps students get apartments", "Location based tutoring service", "Growth hacking for startups"],
-    correctAnswer: 1;
-    userAnswer: 0;
+    correctAnswer: 1,
+    userAnswer: null
   };
 
   var q5 = {
     question: "What is his favorite music genre?",
     answers: ["Anything by Frank Sinatra", "Electronic dance music", "What's on the radio", "He beez in da trap"],
-    correctAnswer: 1;
-    userAnswer: 0;
+    correctAnswer: 1,
+    userAnswer: null
   };
 
   var q6 = {
     question: "Who is his favorite startup writer?",
     answers: ["Sam Altman", "Nivi & Naval", "Paul Graham", "Ben Horowitz"],
-    correctAnswer: 2;
-    userAnswer: 0;
+    correctAnswer: 2,
+    userAnswer: null
   };
 
   var q7 = {
     question: "Who is his favorite CEO?",
     answers: ["Travis Kalanick (Uber)", "Aaron Levie (Box)", "Mark Zuckerberg (Facebook)", "Elon Musk (Tesla, SpaceX)"],
-    correctAnswer: 1;
-    userAnswer: 0;
+    correctAnswer: 1,
+    userAnswer: null
   };
 
   var q8 = {
     question: "What is he learning now outside of Front-end and Ruby on Rails?",
     answers: ["Javascript frameworks", "Design", "Growth hacking", "Raising money"],
-    correctAnswer: 2;
-    userAnswer: 0;
+    correctAnswer: 2,
+    userAnswer: null
   };
 
   var q9 = {
     question: "Where is he looking to transfer this fall for school?",
     answers: ["University of Pennslyvania", "University of Maryland", "University of Illinois", "Rutgers University"],
-    correctAnswer: 0;
-    userAnswer: 0;
+    correctAnswer: 0,
+    userAnswer: null
   };
 
   var q10 = {
     question: "What does he want to do in 2014?",
     answers: ["Get into YCombinator", "Get funded by Dorm Room Fund", "Have his startup at 5 schools", "He is just that greedy and wants it all"],
-    correctAnswer: 3;
-    userAnswer: 0;
+    correctAnswer: 3,
+    userAnswer: null
   };
 
   var quizQuestions = [q1, q2, q3, q4, q5, q6, q7, q8, q9, q10];
@@ -77,10 +76,21 @@ function MikeAdeleke() {
 
 
 $(document).ready(function() {
-  var quizName = new MikeAdeleke();
+  var mikeAdeleke = {
+    quizName: "Mike Adeleke"
+  };
 
-  var click = $("#button").on("click", function {
-    alert("Oh hai there!");
-  });
+  $("#button").on("click", function() {
+    alert("Hello there!");
+  })
 
 })
+
+
+
+
+// Create the questions first
+// Click button
+// Submit handler
+// Add html that is hidden
+
